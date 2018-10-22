6a3e9fac664a84edb52a0c7415fce4f02d194720
Ádám Eperjesi <edamo@freemail.hu>
2018-10-22 12:56:43 +0200

    Complete refactor

--- keno.py
+++ keno.py
@@ -1,74 +1,121 @@
 import csv
-import dask.dataframe as dd
+import os
+from xsorted import xsorter
 
-
-DAY_LIMIT = 3000
+HEADER_FIELDS = ['numbers', 'frequency']
+HEADER = ';'.join(HEADER_FIELDS)
+DAY_LIMIT = 99999
 
 
 def process_row(current_row):
     results = []
-    for a in range(0, 16):
-        for b in range(a + 1, 17):
-            for c in range(b + 1, 18):
-                for d in range(c + 1, 19):
-                    for e in range(d + 1, 20):
-                        results.append(
-                            "{},{},{},{},{}".format(current_row[a], current_row[b], current_row[c],
-                                                       current_row[d], current_row[e]))
+    for a in range(0, 15):
+        for b in range(a + 1, 16):
+            for c in range(b + 1, 17):
+                for d in range(c + 1, 18):
+                    for e in range(d + 1, 19):
+                        for f in range(e + 1, 20):
+                            results.append(
+                                "{},{},{},{},{},{}".format(current_row[a], current_row[b], current_row[c],
+                                                           current_row[d], current_row[e], current_row[f]))
 
     return results
 
 
-def select_max(grouped_df):
-    row_with_max_cnt_index = grouped_df['frequency'].idmax()
-    row_with_max_cnt = grouped_df.loc[row_with_max_cnt_index]
-    return row_with_max_cnt
-
-
-build_input = True
+build_input = False
 result_dict = {}
-input_csv = csv.reader(open('keno.csv'), delimiter=';')
-
-row_counter = 0
-values_string_list = []
 
 if build_input:
-    with (open("results.csv", "w")) as result_file:
-        print("numbers;frequency", file=result_file)
-        for row in input_csv:
-            row_counter += 1
-            if row_counter > DAY_LIMIT:
-                result_dict.clear()
-                break
+    print("Building dictionary...")
 
-            for result in process_row(row[4:]):
-                if result not in result_dict:
-                    result_dict[result] = 1
-                else:
-                    result_dict[result] += 1
+    with (open('keno.csv', "r")):
+        input_csv = csv.reader(open('keno.csv'), delimiter=';')
+        with (open("data_dump.csv", "w")) as data_dump:
+            # Write header
+            print(HEADER, file=data_dump)
 
-            if row_counter % 100 == 0:
-                for k, v in result_dict.items():
-                    print("{};{}".format(k, v), file=result_file)
+            for row_number, row in enumerate(input_csv):
+                if row_number > DAY_LIMIT:
+                    break
 
-                print("{} rows processed".format(row_counter))
-                result_dict.clear()
+                if row_number > 0 and row_number % 100 == 0:
+                    for k, v in result_dict.items():
+                        print("{};{}".format(k, v), file=data_dump)
+                    result_dict.clear()
+                    print("{} rows processed".format(row_number))
+
+                for result in process_row(row[4:]):
+                    if result not in result_dict:
+                        result_dict[result] = 1
+                    else:
+                        result_dict[result] += 1
+
+            for k, v in result_dict.items():
+                print("{};{}".format(k, v), file=data_dump)
+            result_dict.clear()
 else:
     print("Skipping input csv building...")
 
 
-print("Creating DataFrame...")
-df = dd.read_csv("results.csv", sep=";", dtype={'numbers': str, 'frequency': int})
-#input_df = pd.read_csv("results.csv", sep=";", names=["numbers", "frequency"])
+print("Sorting results by key...")
+with open("data_dump.csv", "r") as data_dump:
+    reader = csv.DictReader(data_dump, delimiter=';')
+    xsorter_custom = xsorter(partition_size=131072)
+    sorted_items = xsorter_custom(reader, key=lambda f: f['numbers'])
+    with open("data_dump_sorted.csv", "w") as data_dump_sorted:
+        writer = csv.DictWriter(data_dump_sorted, reader.fieldnames, delimiter=';', lineterminator='\n')
+        writer.writeheader()
+        for item in sorted_items:
+            writer.writerow(item)
 
-print("Sorting dataframe...")
-grouped_df = df.groupby("numbers").sum().reset_index()
-grouped_df.set_index("frequency")
-grouped_df = grouped_df.map_partitions(lambda x: x.sort_index())
-print(grouped_df.compute())
-print(grouped_df.head(10))
-print(grouped_df.tail(10))
+print("Merging results...")
 
-#sorted_results = [(k, result_dict[k]) for k in sorted(result_dict, key=result_dict.get, reverse=True)]
-#for k, v in sorted_results[:10]:
-    #print("{} Húzások száma: {}".format(k, v))
+# Delete previous merged file if exists
+if os.path.exists("merged_data.csv"):
+    os.remove("merged_data.csv")
+
+with open("data_dump_sorted.csv") as data_dump_sorted:
+    data_dump_reader = csv.DictReader(data_dump_sorted, delimiter=";")
+    with open("merged_data.csv", "w") as merged_data:
+        merged_results_writer = csv.DictWriter(merged_data, data_dump_reader.fieldnames, delimiter=";", lineterminator='\n')
+        merged_results_writer.writeheader()
+
+        unique_key_counter = 0
+        dict_for_chunks = {}
+
+        for row_number, row in enumerate(data_dump_reader):
+            key = row["numbers"]
+            value = int(row["frequency"])
+            if key not in dict_for_chunks:
+                if unique_key_counter > 100:
+                    merged_results_writer.writerows({'numbers': k, 'frequency': v} for k, v in dict_for_chunks.items())
+                    unique_key_counter = 0
+                    dict_for_chunks.clear()
+
+                unique_key_counter += 1
+                dict_for_chunks[key] = value
+            else:
+                dict_for_chunks[key] += value
+
+        if len(dict_for_chunks) != 0:
+            merged_results_writer.writerows({'numbers': k, 'frequency': v} for k, v in dict_for_chunks.items())
+
+print("Sorting results by value...")
+with open("merged_data.csv", "r") as merged_data:
+    reader = csv.DictReader(merged_data, delimiter=';')
+    xsorter_custom = xsorter(partition_size=131072)
+    sorted_items = xsorter_custom(reader, key=lambda f: f['frequency'], reverse=True)
+    with open("merged_data_sorted.csv", "w") as merged_data_sorted:
+        writer = csv.DictWriter(merged_data_sorted, reader.fieldnames, delimiter=';', lineterminator='\n')
+        writer.writeheader()
+        for item in sorted_items:
+            writer.writerow(item)
+
+with open("merged_data_sorted.csv", "r") as final_result:
+    reader = csv.DictReader(final_result, delimiter=';')
+    for i, row in enumerate(reader):
+        print("{} : {}".format(row['numbers'], row['frequency']))
+        if i > 10:
+            break
+
+
